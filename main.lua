8dd9143845cddb94798788bba8ee59a8833142c3
Bianca-Alexandra Casapu <bianca-alexandra.casapu17@imperial.ac.uk>
2019-01-26 17:41:05 +0000

    smooth moving

--- main.lua
+++ main.lua
@@ -1,12 +1,18 @@
 require("conf")
 
 GROUNDHEIGHT = 500;
+SPEED = 200;
+SPEED_DEC = 3;
 
 player = {
     width = 100;
     height = 100;
     x = 400;
     y = GROUNDHEIGHT - 100;
+    speed = {
+      x = 0;
+      y = 0;
+    }
 }
 
 function love.load()
@@ -14,7 +20,24 @@ function love.load()
 end
 
 function love.update(dt)
+  
+  if isMovingLeft() then
+    player.speed.x = math.min(0, player.speed.x + SPEED_DEC)
+  elseif isMovingRight() then
+    player.speed.x = math.max(0, player.speed.x - SPEED_DEC)
+  end
 
+  checkKeys(dt)
+
+  player.x = player.x + player.speed.x * dt
+end
+
+function isMovingLeft()
+  return player.speed.x < 0
+end
+
+function isMovingRight()
+  return player.speed.x > 0
 end
 
 function love.draw()
@@ -34,6 +57,14 @@ function drawGround()
 end
 
 function drawPlayer()
-  love.graphics.setColor(0,0,0,1)
+  love.graphics.setColor(0.02, 0.3, 0.8, 1)
   love.graphics.rectangle("fill", player.x, player.y, player.height, player.width)
+end
+
+function checkKeys(dt)
+  if love.keyboard.isDown("left") then
+    player.speed.x = -SPEED
+  elseif love.keyboard.isDown("right") then
+    player.speed.x = SPEED
+  end
 end
