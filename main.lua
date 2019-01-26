13f1b0350355ea1a43ddde5c4291336274d0b2b4
Calin Farcas <cf316@ic.ac.uk>
2019-01-26 18:25:01 +0000

    implemented screen hovering

--- main.lua
+++ main.lua
@@ -2,18 +2,30 @@ require("conf")
 
 GROUNDHEIGHT = 500;
 SPEED = 200;
-SPEED_DEC = 3;
-CAMERA_TRESHOLD = 200;
+SPEED_DEC = 10;
+CAMERA_TRESHOLD = 300;
+CAMERA_THRESHOLD_LEFT = CAMERA_TRESHOLD
+CAMERA_THRESHOLD_RIGHT = WIDTH - CAMERA_TRESHOLD
 
 player = {
     width = 100;
     height = 100;
     x = 400;
     y = GROUNDHEIGHT - 100;
+    draw_x = 400;
+    draw_y = GROUNDHEIGHT - 100;
     speed = {
       x = 0;
       y = 0;
     }
+}
+
+ground = {
+    width = WIDTH - 200;
+    height = 100;
+    x = 400;
+    y = HEIGHT - GROUNDHEIGHT;
+    draw_x = 0;
 }
 
 function love.load()
@@ -30,7 +42,9 @@ function love.update(dt)
 
   checkKeys(dt)
 
+  local original_x = player.x;
   player.x = player.x + player.speed.x * dt
+    updateDrawCoords(player.x - original_x)
 end
 
 function isMovingLeft()
@@ -54,19 +68,29 @@ end
 
 function drawGround()
   love.graphics.setColor(0,0,0,1)
-  love.graphics.rectangle("fill", WIDTH / 2 - player.x, GROUNDHEIGHT, WIDTH, 100)
+  love.graphics.rectangle("fill", ground.draw_x, GROUNDHEIGHT, WIDTH, ground.height)
 end
 
 function drawPlayer()
   love.graphics.setColor(0.02, 0.3, 0.8, 1)
-  love.graphics.rectangle("fill", 400 - player.width / 2, player.y, player.height, player.width)
+  love.graphics.rectangle("fill", player.draw_x, player.y, player.height, player.width)
 end
 
-function checkKeys(dt)
+function checkKeys()
   if love.keyboard.isDown("left") then
     player.speed.x = -SPEED
   elseif love.keyboard.isDown("right") then
     player.speed.x = SPEED
   end
+end
+
+function updateDrawCoords(delta)
+    if CAMERA_THRESHOLD_LEFT < player.draw_x + player.width / 2 + delta and player.draw_x + player.width / 2 + delta < CAMERA_THRESHOLD_RIGHT then
+        -- move player
+        player.draw_x = player.draw_x + delta
+    else
+        -- move ground
+        ground.draw_x = ground.draw_x - delta
+    end
 end
 
