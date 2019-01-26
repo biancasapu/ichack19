ce7f39c1c677c33cfa5ebf8e7ca18d4413d1c6b7
Bianca-Alexandra Casapu <bianca-alexandra.casapu17@imperial.ac.uk>
2019-01-26 16:48:24 +0000

    added player

--- main.lua
+++ main.lua
@@ -1,5 +1,14 @@
 require("conf")
 
+GROUNDHEIGHT = 500;
+
+player = {
+    width = 100;
+    height = 100;
+    x = 400;
+    y = GROUNDHEIGHT - 100;
+}
+
 function love.load()
   
 end
@@ -11,6 +20,7 @@ end
 function love.draw()
   drawBackground()
   drawGround()
+  drawPlayer()
 end
 
 function drawBackground()
@@ -20,5 +30,10 @@ end
 
 function drawGround()
   love.graphics.setColor(0,0,0,1)
-  love.graphics.rectangle("fill", 0, 500, WIDTH, 100)
+  love.graphics.rectangle("fill", 0, GROUNDHEIGHT, WIDTH, 100)
+end
+
+function drawPlayer()
+  love.graphics.setColor(0,0,0,1)
+  love.graphics.rectangle("fill", player.x, player.y, player.height, player.width)
 end
